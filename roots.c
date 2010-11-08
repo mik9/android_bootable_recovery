104ca7da88601389dd6df68fcf10da3f8187ce4b
LeshaK <leshak@leshak.ru>
2010-11-08 23:12:11 +0300

    fixed format internal

--- roots.c
+++ roots.c
@@ -402,7 +402,7 @@ format_root_device(const char *root)
         LOGW("format_root_device: can't resolve \"%s\"\n", root);
         return -1;
     }
-    if (info->mount_point != NULL && info->device == g_mtd_device) {
+    if (info->mount_point != NULL) {
         /* Don't try to format a mounted device.
          */
         int ret = ensure_root_path_unmounted(root);
