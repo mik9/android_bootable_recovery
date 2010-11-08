8405e95cf6e4a2dcc1e9804abee29c76a2de06da
LeshaK <leshak@leshak.ru>
2010-11-08 22:27:54 +0300

    remove wipe SDEXT

--- recovery.c
+++ recovery.c
@@ -448,7 +448,6 @@ wipe_data(int confirm) {
     erase_root("DATADATA:");
 #endif
     erase_root("CACHE:");
-    erase_root("SDEXT:");
     erase_root("SDCARD:/.android_secure");
     ui_print("Data wipe complete.\n");
 }
